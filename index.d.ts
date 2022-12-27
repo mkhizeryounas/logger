fc0df2f9a265917384358a47bb69d02c4b0a3c87
Muhammad Khizer Younas <khizer@fabric.inc>
2022-12-27 15:46:28 -0800

    doc: documentation added

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+const logFn = function(log:string):void {};
+
+export const debug = logFn;
+export const log = logFn;
+export const info = logFn;
+export const warn = logFn;
+export const error = logFn;
